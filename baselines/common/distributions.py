2e0c01d89ec23bc6c0f5a1286fabf4767b998e53
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-01-16 15:40:38 -0800

    Go back to Gaussian noise

--- baselines/common/distributions.py
+++ baselines/common/distributions.py
@@ -301,14 +301,14 @@ class BernoulliPd(Pd):
 
 def make_pdtype(ac_space):
     from gym import spaces
-    if isinstance(ac_space, spaces.Box) and ac_space.shape == (4,): # try to fetech Fetch
+    # if isinstance(ac_space, spaces.Box) and ac_space.shape == (4,): # try to fetech Fetch
+    #     assert len(ac_space.shape) == 1
+    #     return OUPdType(ac_space.shape[0])
+    if isinstance(ac_space, spaces.Box):
+        # print("Warning: about to instantiate continuous policy without Ornstein-Uhlenbeck noise. Is this really what you want?")
+        # exit(1)
         assert len(ac_space.shape) == 1
-        return OUPdType(ac_space.shape[0])
-    elif isinstance(ac_space, spaces.Box):
-        print("Warning: about to instantiate continuous policy without Ornstein-Uhlenbeck noise. Is this really what you want?")
-        exit(1)
-        # assert len(ac_space.shape) == 1
-        # return DiagGaussianPdType(ac_space.shape[0])
+        return DiagGaussianPdType(ac_space.shape[0])
     elif isinstance(ac_space, spaces.Discrete):
         return CategoricalPdType(ac_space.n)
     elif isinstance(ac_space, spaces.MultiDiscrete):
