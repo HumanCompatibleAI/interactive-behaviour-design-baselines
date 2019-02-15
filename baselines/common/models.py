e94cdf6f7f4caeb11c6c4c42bc24ea3670c7eef6
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-02-15 13:53:09 -0800

    Log network setup

--- baselines/common/models.py
+++ baselines/common/models.py
@@ -46,6 +46,7 @@ def mlp(num_layers=2, num_hidden=64, activation=tf.tanh, layer_norm=False):
 
     function that builds fully connected network with a given input tensor / placeholder
     """
+    print("mlp with", locals())
     def network_fn(X):
         h = tf.layers.flatten(X)
         for i in range(num_layers):
